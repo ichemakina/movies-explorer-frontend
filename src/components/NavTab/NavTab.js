9a5e066f86f5e91f2efef2400b429031b17a97e2
ichemakina <themackinaira@yandex.ru>
2024-01-05 20:32:51 +0300

    Добавляет шапку для авторизованного пользователя

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,14 +1,28 @@
 import logo from "../../images/logo.svg";
 import "./NavTab.css";
+import account from "../../images/account.svg";
 
-function NavTab() {
+function NavTab({ authorized = false }) {
     return (
         <nav className="nav">
             <img className="nav__logo" src={logo} alt="С"></img>
-            <ul className="nav__links">
-                <li className="nav__link">Регистрация</li>
-                <li className="nav__link"><button className="nav__button">Войти</button></li>
-            </ul>
+            {authorized
+                ? (<>
+                    <ul className="nav__links nav__links_authorized">
+                        <li className="nav__link">Фильмы</li>
+                        <li className="nav__link">Сохранённые фильмы</li>
+                    </ul>
+                    <div className="nav__links nav__links_authorized nav__links_type_account">
+                        <p className="nav__link">Аккаунт</p>
+                        <a className="nav__link nav__link_type_account" href="#"><img className="nav__account-img" src={account} alt="Аккаунт"></img></a>
+                       
+                    </div>
+                </>
+                )
+                : <ul className="nav__links">
+                    <li className="nav__link">Регистрация</li>
+                    <li className="nav__link"><button className="nav__button">Войти</button></li>
+                </ul>}
         </nav>
     )
 }
