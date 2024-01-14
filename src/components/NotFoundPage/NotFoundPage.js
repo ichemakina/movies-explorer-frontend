a32406109b64ec2457d724650a930c67a84b24fa
ichemakina <themackinaira@yandex.ru>
2024-01-14 11:27:33 +0300

    Доработка кнопки "Назад". Правки по странице NotFound.

--- src/components/NotFoundPage/NotFoundPage.js
+++ src/components/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,14 @@
 import "./NotFoundPage.css";
+import { useNavigate } from "react-router-dom";
 
 function NotFoundPage() {
+    const navigate = useNavigate();
+
     return (
         <section className="not-found-page">
             <h1 className="not-found-page__error-code">404</h1>
             <h2 className="not-found-page__error-text">Страница не найдена</h2>
-            <a className="not-found-page__back" href="/">Назад</a>
+            <button className="not-found-page__back" href="/" onClick={() => navigate(-1)}>Назад</button>
         </section>
     )
 }
