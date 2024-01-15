935fdb8a4305f5a6b67f2c66fb9eca706c400ebf
ichemakina <themackinaira@yandex.ru>
2024-01-15 20:19:17 +0300

    Переделывает бургер-меню. Затемняет фон при его открытии.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,25 +1,33 @@
 import "./Navigation.css";
 import account from "../../images/account.svg";
+import { useState } from 'react';
 
 function Navigation({ isMainPage = false, pageUrl }) {
+    const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+    function handleBurgerMenuClick() {
+        setIsOpenMenu(!isOpenMenu);
+    }
+
     return (
         <nav className="navigation">
-            <input className="navigation__btn" id="menu-toggle" type="checkbox" />
-            <label className="navigation__btn-lines" htmlFor="menu-toggle">
-                <span className="navigation__btn-line"></span>
-            </label>
-
-            <div className="navigation__links">
-                <ul className="navigation__links_type_films">
-                    <li><a className={`navigation__link navigation__link_type_main ${pageUrl === "/" && 'navigation__link_active'}`} href="/">Главная</a></li>
-                    <li><a className={`navigation__link ${pageUrl === "/movies" && 'navigation__link_active'}`} href="/movies">Фильмы</a></li>
-                    <li><a className={`navigation__link ${pageUrl === "/saved-movies" && 'navigation__link_active'}`} href="/saved-movies">Сохранённые фильмы</a></li>
-                </ul>
-                <div className="navigation__links_type_account">
-                    <a className="navigation__link" href="/profile">Аккаунт</a>
-                    <a className={isMainPage ? "navigation__link navigation__link_type_account navigation__link_on-main-page" : "navigation__link navigation__link_type_account"} href="/profile">
-                        <img className="navigation__account-img" src={account} alt="Аккаунт"></img>
-                    </a>
+            <button type="button" className="navigation__open-btn" onClick={handleBurgerMenuClick}></button>
+            <div className={`navigation__menu ${isOpenMenu && 'navigation__menu_opened'}`}>
+                <div className="navigation__close-btn-and-links">
+                    <button type="button" className="navigation__close-btn" onClick={handleBurgerMenuClick}></button>
+                    <div className="navigation__links">
+                        <ul className="navigation__links_type_films">
+                            <li><a className={`navigation__link navigation__link_type_main ${pageUrl === "/" && 'navigation__link_active'}`} href="/">Главная</a></li>
+                            <li><a className={`navigation__link ${pageUrl === "/movies" && 'navigation__link_active'}`} href="/movies">Фильмы</a></li>
+                            <li><a className={`navigation__link ${pageUrl === "/saved-movies" && 'navigation__link_active'}`} href="/saved-movies">Сохранённые фильмы</a></li>
+                        </ul>
+                        <div className="navigation__links_type_account">
+                            <a className="navigation__link" href="/profile">Аккаунт</a>
+                            <a className={isMainPage ? "navigation__link navigation__link_type_account navigation__link_on-main-page" : "navigation__link navigation__link_type_account"} href="/profile">
+                                <img className="navigation__account-img" src={account} alt="Аккаунт"></img>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
