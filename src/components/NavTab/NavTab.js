bb1a7bfb63ba3528027d094dda6d00264f522176
ichemakina <themackinaira@yandex.ru>
2024-01-18 17:54:49 +0300

    Убирает лишний className

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -5,7 +5,7 @@ function NavTab() {
         <nav>
             <ul className="header__nav-links">
                 <a className="header__nav-link" href="/signup"><li>Регистрация</li></a>
-                <a className="header__nav-link" href="/signin"><li className="nav__link"><button type="button" className="header__nav-button">Войти</button></li></a>
+                <a className="header__nav-link" href="/signin"><li><button type="button" className="header__nav-button">Войти</button></li></a>
             </ul>
         </nav>
     )
