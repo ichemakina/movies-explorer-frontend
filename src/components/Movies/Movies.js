317a150326307c3259ef56babb3137513c3e54fc
ichemakina <themackinaira@yandex.ru>
2024-01-22 18:47:11 +0300

    Количество отображаемых карточек зависит от ширины экрана устройства

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,15 +4,47 @@ import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import SearchForm from "../SearchForm/SearchForm";
 import Preloader from "../Preloader/Preloader.js";
 import "./Movies.css";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { moviesApi } from "../../utils/MoviesApi.js";
 import NotSearchResults from "../NotSearchResults/NotSearchResults.js";
 
 function Movies({ pageUrl }) {
     const [searchResults, setSearchResults] = useState([]);
+    const [displayedMovies, setDisplayedMovies] = useState([]);
     const [errors, setErrors] = useState('');
     const [preloader, setPreloader] = useState(false);
     const [notSearchResults, setNotSearchResults] = useState(false);
+    const [width, setWidth] = useState(window.innerWidth);
+    const [moviesCount, setMoviesCount] = useState([]);
+
+    useEffect(() => {
+        function getMoviesCount() {
+            if (width > 840)
+                return 16;
+            if (width > 580)
+                return 8;
+            return 5;
+        }
+
+        setMoviesCount(getMoviesCount());
+        const handleResize = (event) => {
+            setWidth(event.target.innerWidth);
+            setMoviesCount(getMoviesCount());
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [width]);
+
+    useEffect(() => {
+        getDisplayedMovies(searchResults)
+    }, [searchResults, width]);
+
+    function getDisplayedMovies(movies) {
+        const displayedMovies = movies.slice(0, moviesCount);
+        setDisplayedMovies(displayedMovies);
+    }
 
     function handleSearch(searchValue) {
         if (!searchValue) {
@@ -30,10 +62,12 @@ function Movies({ pageUrl }) {
                     movie.nameRU.toLowerCase().includes(searchValue.toLowerCase())
                 );
                 setSearchResults(results);
-                if (results.length === 0)
+                if (movies.length === 0) {
                     setNotSearchResults(true);
+                }
                 else
                     setNotSearchResults(false);
+                getDisplayedMovies(results);
                 setErrors('');
             })
             .catch(() => {
@@ -51,7 +85,7 @@ function Movies({ pageUrl }) {
                 <SearchForm handleSearch={handleSearch} errors={errors} />
                 {preloader && <Preloader />}
                 {notSearchResults && <NotSearchResults />}
-                <MoviesCardList movies={searchResults} />
+                {displayedMovies.length !== 0 && <MoviesCardList movies={displayedMovies} />}
             </main>
             <Footer />
         </div>
